0254760211af971f9ca920ac9a3360b8d00762d0
Laura Sirianni <lausirianni@hotmail.com>
2017-11-26 23:40:12 -0200

    Setting and changing state through method

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,21 @@ import React, { Component } from 'react';
 import './App.css'
 
 class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      deadline: 'Countdown to December, 25, 2017'
+    }
+  }
+
+  changeDeadline(){
+    this.setState({deadline: 'Contador para 25 de Dezembro de 2017'})
+  }
+
   render(){
     return(
       <div className="app">
-        <div className="app-title">Countdown to December 25, 2017</div>
+        <div className="app-title">{this.state.deadline}</div>
           <div>
             <div className="clock-days">14 days</div>
             <div className="clock-hours">30 hours</div>
@@ -13,7 +24,7 @@ class App extends Component{
             <div className="clock-seconds">20 seconds</div>
           </div>
         <input placeholder="new date"/>
-        <button>Submit</button>
+        <button onClick={() => this.changeDeadline()}>Submit</button>
       </div>
     )
   }
