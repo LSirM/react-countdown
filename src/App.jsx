3bd46d19a841ac856530e00dfa5509e3459a1ae1
Laura Sirianni <lausirianni@hotmail.com>
2017-11-29 01:37:33 -0200

    Added clock countdown

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,27 @@
 import React, { Component } from 'react';
-import './App.css'
+import Clock from './Clock';
+import './App.css';
 
 class App extends Component{
   constructor(props){
     super(props);
     this.state = {
-      deadline: 'Countdown to December, 25, 2017'
+      deadline: 'December 25, 2017',
+      newDeadline: ''
     }
   }
 
   changeDeadline(){
-    this.setState({deadline: 'Contador para 25 de Dezembro de 2017'})
+    this.setState({deadline: this.state.newDeadline})
+    console.log('state', this.state)
   }
 
   render(){
     return(
       <div className="app">
-        <div className="app-title">{this.state.deadline}</div>
-          <div>
-            <div className="clock-days">14 days</div>
-            <div className="clock-hours">30 hours</div>
-            <div className="clock-minutes">15 minutes</div>
-            <div className="clock-seconds">20 seconds</div>
-          </div>
-        <input placeholder="new date"/>
+        <div className="app-title">Countdown to {this.state.deadline}</div>
+        <Clock deadline={this.state.deadline}/>
+        <input placeholder="new date" onChange={event => this.setState({newDeadline: event.target.value})}/>
         <button onClick={() => this.changeDeadline()}>Submit</button>
       </div>
     )
